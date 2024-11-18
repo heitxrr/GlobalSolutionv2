ba9b26f79acf6c4dc8bfa623842c82e5dc656ed1
heitxrr <heitorrrocha04@outlok.com>
2024-11-17 23:09:30 -0300

    Criando um rodapé

---
+++ eficientiza/components/Rodape/Rodape.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { RodapeProps } from "@/types/Rodape";
+import imgLogo from "@/public/images/logo-eficientiza.png";
+
+export default function Rodape (props : RodapeProps) {
+
+  const { gray } = props;
+
+  if (gray) {
+    return (
+      <footer className="flex items-center justify-center max-h-28 h-28 px-2 py-0.5 bg-gray-300">
+        <Image className="block h-full w-auto" src={imgLogo} alt="Logo da Eficientiza" />
+      </footer>
+    )
+  } else {
+    return (
+      <footer className="flex items-center justify-center max-h-28 h-28 px-2 py-0.5">
+        <Image className="block h-full w-auto" src={imgLogo} alt="Logo da Eficientiza" />
+      </footer>
+    )
+  }
+}
