9ee59be9655369908c585a19980ac40b637fbad7
heitxrr <heitorrrocha04@outlok.com>
2024-11-21 14:37:18 -0300

    adicionando tipagem para turma no card de autor

--- eficientiza/types/AutorCard.ts
+++ eficientiza/types/AutorCard.ts
@@ -3,6 +3,7 @@ import { StaticImageData } from 'next/image';
 export type AutorCardProps = {
     img: StaticImageData;
     nome: string;
+    turma: string;
     rm: string;
     usuarioGithub: string;
   }
