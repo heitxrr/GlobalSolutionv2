fc237d549a88ccfe507da1b150323aeff563c306
heitxrr <heitorrrocha04@outlok.com>
2024-11-21 13:13:36 -0300

    Criando carrosséis

--- eficientiza/app/eventos/page.tsx
+++ eficientiza/app/eventos/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import Image from 'next/image';
 
-function EventPage() {
+function DualCarousel() {
   const images = [
     '/images/logo-eficientiza-navbar.png',
     '/images/logo-eficientiza-navbar.png',
@@ -11,28 +11,45 @@ function EventPage() {
     '/images/logo-eficientiza-navbar.png',
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex1, setCurrentIndex1] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(0);
 
-  const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  const nextImage1 = () => {
+    setCurrentIndex1((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  const prevImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  const prevImage1 = () => {
+    setCurrentIndex1((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const nextImage2 = () => {
+    setCurrentIndex2((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage2 = () => {
+    setCurrentIndex2((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   return (
     <main className="flex flex-col items-center">
       <div className="carousel w-full flex justify-center items-center my-10">
-        <button onClick={prevImage} className="p-2">Anterior</button>
+        <button onClick={prevImage1} className="p-2">Anterior</button>
         <div className="flex">
-          <Image src={images[currentIndex]} alt="Event Image 1" width={300} height={300} />
-          <Image src={images[(currentIndex + 1) % images.length]} alt="Event Image 2" width={300} height={300} />
+          <Image src={images[currentIndex1]} alt="Event Image 1" width={300} height={300} />
+          <Image src={images[(currentIndex1 + 1) % images.length]} alt="Event Image 2" width={300} height={300} />
         </div>
-        <button onClick={nextImage} className="p-2">Próximo</button>
+        <button onClick={nextImage1} className="p-2">Próximo</button>
+      </div>
+      <div className="carousel w-full flex justify-center items-center my-10">
+        <button onClick={prevImage2} className="p-2">Anterior</button>
+        <div className="flex">
+          <Image src={images[currentIndex2]} alt="Event Image 3" width={300} height={300} />
+          <Image src={images[(currentIndex2 + 1) % images.length]} alt="Event Image 4" width={300} height={300} />
+        </div>
+        <button onClick={nextImage2} className="p-2">Próximo</button>
       </div>
     </main>
   );
 }
 
-export default EventPage;
+export default DualCarousel;
